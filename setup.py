97ebbc6a778198982baab1a4da5a3a82a9712f36
mivazq <miguel.vazquezvazquez@econ.uzh.ch>
2024-02-05 18:17:26 +0100

    Edited gitignore to ignore .vscode folder, create setup.py file and first python script to perform network analysis

---
+++ setup.py
@@ -0,0 +1,69 @@
+#///////////////////////////////////////////////////////////////////////////////
+# File name:        setup.py
+# Author:           Miguel Vázquez Vázquez
+# Creation date:    20 November 2023
+# Description:      This file installs packages, defines paths, loads functions
+#///////////////////////////////////////////////////////////////////////////////
+#----                       1 - PACKAGES AND OPTIONS                        ----
+#///////////////////////////////////////////////////////////////////////////////
+
+# Load all needed libraries
+import sys
+import os
+import glob
+import importlib
+import datetime
+import math
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# Store date to save files
+sysdate = datetime.datetime.now().strftime("%Y-%m-%d")
+
+#///////////////////////////////////////////////////////////////////////////////
+#----                               2 - PATHS                               ----
+#///////////////////////////////////////////////////////////////////////////////
+
+# Main paths for Ecuador project
+ecuRaw  = '/home/mivazq/data/transactions_ecuador/1_rawdata/'
+ecuAll  = '/home/mivazq/data/transactions_ecuador/2_shared/'
+ecuFCT  = ecuAll + 'factorContentTrade/'
+ecuMine = '/home/mivazq/data/transactions_ecuador/3_mivazq/'
+
+# Set working directory and add to path
+pathWD = ecuMine + 'Masters_Thesis/'
+os.chdir(pathWD)
+if pathWD not in sys.path:
+    sys.path.append(pathWD)
+
+# Project sub-folders
+pathCle = pathWD + 'cleaning/'
+pathEst = pathWD + 'estimation/'
+pathFun = pathWD + 'functions/'
+
+# Output files
+pathFig = pathWD + 'results/figures/'
+pathTab = pathWD + 'results/tables/'
+pathItx = pathWD + 'results/intext/'
+
+#///////////////////////////////////////////////////////////////////////////////
+#----                           3 - FUNCTIONS                               ----
+#///////////////////////////////////////////////////////////////////////////////
+
+# First, add functions directory to path
+if pathFun not in sys.path:
+    sys.path.append(pathFun)
+
+# If functions folder is not empty, source all Python functions in the folder
+py_files = []
+for file in glob.glob(pathFun+"*.py"):
+    py_files.append(file)
+py_files = [file.split(".")[0] for file in py_files] # remove the extension  
+py_files = [file.split("/")[-1] for file in py_files] # remove the path
+
+if len(py_files) > 0:
+    for file in py_files:
+        globals()[file] = importlib.import_module(file)
